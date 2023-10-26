5d5c86b237a77189b1d66fdb5c111bfd3c26a9e5
Eyader Tsehayu <eyaderts@gmail.com>
2023-10-26 16:35:04 +0300

    add user acc setting

---
+++ app/user/settings/page.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import UserForm from "@/components/user/UserForm";
+
+const Setting = () => {
+  return (
+    <div className="my-12">
+      <UserForm />
+    </div>
+  );
+};
+
+export default Setting;
