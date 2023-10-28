9fb02614ab95260fe3d53a2ae7f6ae1820320cb8
Eyader Tsehayu <eyaderts@gmail.com>
2023-10-28 20:31:50 +0300

    initial validations

---
+++ validations/userValidation.js
@@ -0,0 +1,29 @@
+import { string } from "prop-types";
+import * as yup from "yup";
+
+export const loginSchema = yup.object({
+  id: yup.string().required("Id is required"),
+  password: yup.string().min(8).max(20).required(),
+});
+
+export const personalInfoSchema = yup.object({
+  email: yup.string().email().required("email is required"),
+});
+export const changePasswordSchema = yup.object({
+  oldPassword: yup
+    .string()
+    .required("Old password required")
+    .min(8, "Incorrect password")
+    .max(20, "Incorrect password"),
+
+  newPassword: yup
+    .string()
+    .required("Enter new password")
+    .min(8, "New password must be at least 8 characters")
+    .max(20, "New password must be at most 20 characters"),
+  confirmPassword: yup
+    .string()
+    .required("Re enter  your new password")
+    .min(8, "New password must be at least 8 characters")
+    .max(20, "New password must be at most 20 characters"),
+});
