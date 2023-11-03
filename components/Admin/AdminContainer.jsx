41c3175c6d40af24be95b32c7dd155282db8725c
Eyader Tsehayu <eyaderts@gmail.com>
2023-11-03 20:24:17 +0300

    prepare for calling a modal

--- components/Admin/AdminContainer.jsx
+++ components/Admin/AdminContainer.jsx
@@ -5,8 +5,9 @@ import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
 import Modal from "@mui/material/Modal";
 import Table from "./Table";
+import RegisterStudent from "../Modals/RegisterStudent";
 
-const AdminContainer = ({ columns, rows }) => {
+const AdminContainer = ({ columns, rows, modal: OpenedModal }) => {
   const [open, setOpen] = useState(false);
   const handleOpen = () => setOpen(true);
   const handleClose = () => setOpen(false);
@@ -20,7 +21,7 @@ const AdminContainer = ({ columns, rows }) => {
       <div className="flex w-full justify-between items-center mb-4">
         <div className="flex-grow"></div>
         <div className="flex gap-4 flex-inline  items-center rounded-md  p-1.5 ">
-          <button className="rounded-lg  justify-center  bg-gray hover:bg-meta-1 py-2 px-6 font-medium text-black hover:text-whiten hover:bg-opacity-95">
+          <button className="rounded-lg  justify-center  bg-gray hover:bg-meta-1 py-2 px-6 font-medium text-black dark:bg-meta-4 dark:text-white hover:text-whiten hover:bg-opacity-95 dark:hover:border-meta-1 dark:hover:bg-meta-1">
             Deactivate
           </button>
           <button
@@ -46,29 +47,7 @@ const AdminContainer = ({ columns, rows }) => {
           onClick={handleOverlayClick}
           class="absolute top-0 left-0 z-999999 flex h-full min-h-screen w-full items-center justify-center bg-black/90 px-4 py-5 "
         >
-          <div class="w-full max-w-142.5 rounded-lg bg-white py-12 px-8 text-center dark:bg-boxdark md:py-15 md:px-17.5">
-            <h3 class="pb-2 text-lg font-bold text-black dark:text-white sm:text-2xl">
-              Your Message Sent Successfully
-            </h3>
-            <span class="mx-auto mb-6 inline-block h-1 w-22.5 rounded bg-primary"></span>
-            <p class="mb-10">
-              Lorem Ipsum is simply dummy text of the printing and typesetting
-              industry. Lorem Ipsum has been the industry's standard dummy text
-              ever since
-            </p>
-            <div class="-mx-3 flex flex-wrap gap-y-4">
-              <div class="w-full px-3 2xsm:w-1/2">
-                <button class="block w-full rounded border border-stroke bg-gray p-3 text-center font-medium text-black transition hover:border-meta-1 hover:bg-meta-1 hover:text-white dark:border-strokedark dark:bg-meta-4 dark:text-white dark:hover:border-meta-1 dark:hover:bg-meta-1">
-                  Cancel
-                </button>
-              </div>
-              <div class="w-full px-3 2xsm:w-1/2">
-                <button class="block w-full rounded border border-primary bg-primary p-3 text-center font-medium text-white transition hover:bg-opacity-90">
-                  View Details
-                </button>
-              </div>
-            </div>
-          </div>
+          <OpenedModal />
         </div>
       </Modal>
     </div>
--- components/Admin/Table.jsx
+++ components/Admin/Table.jsx
@@ -1,8 +1,21 @@
+import { Box } from "@mui/material";
 import { DataGrid } from "@mui/x-data-grid";
 
 const Table = ({ columns, rows }) => {
   return (
-    <div style={{ height: 520, width: "100%" }}>
+    <Box
+      style={{ height: 520, width: "100%" }}
+      sx={{
+        "& .MuiDataGrid-footerContainer": {
+          borderTop: "none",
+          backgroundColor: "#34e1eb",
+        },
+        "& .MuiCheckbox-root": {
+          color: "#64748B",
+        },
+      }}
+      className="dark:bg-boxdark-2"
+    >
       <DataGrid
         rows={rows}
         columns={columns}
@@ -13,36 +26,9 @@ const Table = ({ columns, rows }) => {
         }}
         pageSizeOptions={[5, 10]}
         checkboxSelection
-        className="dark:bg-gray-800 dark:text-white "
+        className="dark:bg-gray-800 dark:text-gray "
       />
-      <style>
-        {`.dark .MuiCheckbox-root {
-         
-            border-color: #64748B;
-          
-          }
-          .dark .Mui-checked .MuiIconButton-root {
-            color: #fff; 
-          }
-          .MuiDataGrid-columnHeader {
-          
-           font-size: 1rem;
-          }
-         .MuiDataGrid-pagination {
-            background-color: #333; /* Set the background color of the pagination container */
-            color: #fff; /* Set the text color of the pagination */
-          }
-          .MuiDataGrid-pagination ul li button {
-            background-color: transparent; /* Set the background color of pagination buttons */
-            color: #fff; /* Set the text color of pagination buttons */
-          }
-          .MuiDataGrid-pagination ul li button:hover {
-            background-color: #444; /* Set the background color of pagination buttons on hover */
-          }
-          
-        `}
-      </style>
-    </div>
+    </Box>
   );
 };
 
