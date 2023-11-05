4653a455c490c36be8d7514a22d01af0c5b149ea
Eyader Tsehayu <eyaderts@gmail.com>
2023-11-05 20:26:06 +0300

    validation for modal forms

--- components/Modals/RegisterStudent.jsx
+++ components/Modals/RegisterStudent.jsx
@@ -1,7 +1,7 @@
 "use client";
 import { useState, useRef, useEffect } from "react";
 import { yupResolver } from "@hookform/resolvers/yup";
-import { personalInfoSchema } from "@/validations/userValidation";
+import { registerStudentSchema } from "@/validations/registrationValidation";
 import { useForm } from "react-hook-form";
 import { toast } from "react-toastify";
 
@@ -20,7 +20,16 @@ const collegeData = [
   // Add more office data here
 ];
 const RegisterStudent = () => {
+  const {
+    handleSubmit,
+    register,
+    formState: { errors },
+    reset,
+    setValue,
+  } = useForm({ resolver: yupResolver(registerStudentSchema) });
+
   const Items = [1, 2, 3, 4, 5, 6, 7];
+  const programs = ["Regular", "Weekend"];
 
   const [searchTerm, setSearchTerm] = useState("");
   const [searchCollege, setSearchCollege] = useState("");
@@ -98,6 +107,8 @@ const RegisterStudent = () => {
     setShowDropdown(true);
   };
   const handleDropdownItemClick = (officeName) => {
+    setValue("departmentName", officeName);
+
     setSearchTerm(officeName);
     setShowDropdown(false);
   };
@@ -107,15 +118,11 @@ const RegisterStudent = () => {
   };
 
   const handleDropdownCollegeClick = (collegeName) => {
+    setValue("collegeName", collegeName);
+
     setSearchCollege(collegeName);
     setShowCollegeDropdown(false);
   };
-  const {
-    handleSubmit,
-    register,
-    formState: { errors },
-    reset,
-  } = useForm({ resolver: yupResolver(personalInfoSchema) });
 
   const onSubmit = (data) => {
     console.log(data);
@@ -146,6 +153,7 @@ const RegisterStudent = () => {
               placeholder="student's name"
               {...register("firstName")}
             />
+            <p>{errors.firstName?.message}</p>
           </div>
 
           <div className="w-full sm:w-1/2">
@@ -159,11 +167,12 @@ const RegisterStudent = () => {
               <input
                 className="w-full rounded border border-stroke bg-gray py-3  px-4.5 text-black focus:border-primary focus-visible:outline-none dark:border-strokedark dark:bg-meta-4 dark:text-white dark:focus:border-primary"
                 type="text"
-                name="middlename"
-                id="middlename"
+                name="middleName"
+                id="middleName"
                 placeholder="Father's name"
-                {...register("middlename")}
+                {...register("middleName")}
               />
+              <p>{errors.middleName?.message}</p>
             </div>
           </div>
         </div>
@@ -183,6 +192,7 @@ const RegisterStudent = () => {
               placeholder="Grand father's Name"
               {...register("lastName")}
             />
+            <p>{errors.lastName?.message}</p>
           </div>
 
           <div className="w-full sm:w-1/2">
@@ -200,6 +210,7 @@ const RegisterStudent = () => {
               placeholder=".../..../.."
               {...register("studentId")}
             />
+            <p>{errors.studentId?.message}</p>
           </div>
         </div>
         <div className="mb-5.5 flex flex-col gap-5.5 sm:flex-row">
@@ -209,12 +220,17 @@ const RegisterStudent = () => {
             </label>
             <input
               type="text"
+              name="collegeName"
+              id="collegeName"
               placeholder="Search for a college..."
               value={searchCollege}
               onFocus={handleSearchCollegeFocus}
               onChange={handleSearchCollegeChange}
               className="w-full rounded border border-stroke bg-gray py-3 px-4.5 text-black focus:border-primary focus-visible:outline-none dark:border-strokedark dark:bg-meta-4 dark:text-white dark:focus:border-primary"
+              //   {...register("collegeName")}
             />
+            <p>{errors.collegeName?.message}</p>
+
             {showCollegeDropdown && (
               <div
                 ref={collegeDropdownRef} // Use the college dropdown ref
@@ -242,18 +258,21 @@ const RegisterStudent = () => {
             </label>
             <input
               type="text"
+              name="departmentName"
+              id="departmentName"
               placeholder="Search for an office..."
               value={searchTerm}
               onFocus={handleSearchInputFocus}
               onChange={handleSearchInputChange}
               className="w-full rounded border border-stroke bg-gray py-3 px-4.5 text-black focus:border-primary focus-visible:outline-none dark:border-strokedark dark:bg-meta-4 dark:text-white dark:focus:border-primary"
+              //  {...register("departmentName")}
             />
+            <p>{errors.departmentName?.message}</p>
+
             {showDropdown && (
               <div
                 ref={dropdownRef}
                 className="w-full py-1 rounded-md  border border-stroke bg-gray  text-black focus:border-primary focus-visible:outline-none dark:border-strokedark dark:bg-meta-4 dark:text-white dark:focus:border-primary"
-
-                //  className="absolute mt-2 w-full bg-white border border-gray-300 rounded-md shadow-md max-h-48 overflow-y-auto z-10"
               >
                 {filteredOffices.map((office) => (
                   <div
@@ -268,78 +287,22 @@ const RegisterStudent = () => {
             )}
           </div>
         </div>
-        {/* <div className="relative">
-          <label className="mb-3 block text-sm font-medium text-black dark:text-white">
-            College
-          </label>
-          <input
-            type="text"
-            placeholder="Search for a college..."
-            value={searchCollege}
-            onFocus={handleSearchCollegeFocus}
-            onChange={handleSearchCollegeChange}
-            className="w-full rounded border border-stroke bg-gray py-3 px-4.5 text-black focus:border-primary focus-visible:outline-none dark:border-strokedark dark:bg-meta-4 dark:text-white dark:focus:border-primary"
-          />
-          {showCollegeDropdown && (
-            <div
-              ref={collegeDropdownRef} // Use the college dropdown ref
-              className="w-full py-1 rounded-md  border border-stroke bg-gray  text-black focus:border-primary focus-visible:outline-none dark:border-strokedark dark:bg-meta-4 dark:text-white dark:focus:border-primary"
-            >
-              {filteredColleges.map((college) => (
-                <div
-                  key={college.id}
-                  onClick={() => handleDropdownCollegeClick(college.name)}
-                  className="px-4 py-2 cursor-pointer hover:bg-blue-100 hover:bg-bodydark1 dark:hover:bg-body"
-                >
-                  {college.name}
-                </div>
-              ))}
-            </div>
-          )}
-        </div>
-        <div className="relative">
-          <label
-            className="mb-3 block text-sm font-medium text-black dark:text-white"
-            htmlFor="phoneNumber"
-          >
-            Department
-          </label>
-          <input
-            type="text"
-            placeholder="Search for an office..."
-            value={searchTerm}
-            onFocus={handleSearchInputFocus}
-            onChange={handleSearchInputChange}
-            className="w-full rounded border border-stroke bg-gray py-3 px-4.5 text-black focus:border-primary focus-visible:outline-none dark:border-strokedark dark:bg-meta-4 dark:text-white dark:focus:border-primary"
-          />
-          {showDropdown && (
-            <div
-              ref={dropdownRef}
-              className="w-full py-1 rounded-md  border border-stroke bg-gray  text-black focus:border-primary focus-visible:outline-none dark:border-strokedark dark:bg-meta-4 dark:text-white dark:focus:border-primary"
-
-              //  className="absolute mt-2 w-full bg-white border border-gray-300 rounded-md shadow-md max-h-48 overflow-y-auto z-10"
-            >
-              {filteredOffices.map((office) => (
-                <div
-                  key={office.id}
-                  onClick={() => handleDropdownItemClick(office.name)}
-                  className="px-4 py-2 cursor-pointer hover:bg-blue-100 hover:bg-bodydark1 dark:hover:bg-body"
-                >
-                  {office.name}
-                </div>
-              ))}
-            </div>
-          )}
-        </div> */}
-        <div className="relative">
-          <label className="mb-3 block text-sm font-medium text-black dark:text-white">
+        <div className="relative mb-5.5">
+          <label className=" block text-sm font-medium text-black dark:text-white">
             Year
           </label>
           <div className="grid grid-cols-7">
             {Items.map((item) => (
               <div key={item} className="flex pt-4 min-w-47.5">
                 <span className="mt-1 mr-2 flex h-4 w-full max-w-4 items-center justify-center">
-                  <input className="text-primary" type="radio" />
+                  <input
+                    className="text-primary"
+                    type="radio"
+                    name="year"
+                    id={`year${item}`}
+                    value={item}
+                    {...register("year")}
+                  />
                 </span>
                 <div className="w-full">
                   <p className="font-semibold text-primary dark:text-gray">
@@ -349,32 +312,52 @@ const RegisterStudent = () => {
               </div>
             ))}{" "}
           </div>
+          <p>{errors.year?.message}</p>
+        </div>
+        <div className="relative ">
+          <label className=" block text-sm font-medium text-black dark:text-white">
+            Program
+          </label>
+          <div className="grid grid-cols-4">
+            {programs.map((item) => (
+              <div key={item} className="flex pt-4 min-w-47.5">
+                <span className="mt-1 mr-2 flex h-4 w-full max-w-4 items-center justify-center">
+                  <input
+                    className="text-primary"
+                    type="radio"
+                    name="program"
+                    id={`program${item}`}
+                    {...register("program")}
+                  />
+                </span>
+                <div className="w-full">
+                  <p className="font-semibold text-primary dark:text-gray">
+                    {item}
+                  </p>
+                </div>
+              </div>
+            ))}{" "}
+          </div>
+          <p>{errors.program?.message}</p>
         </div>
 
-        {/* <img
-                  src="https://cdn-icons-png.flaticon.com/512/1721/1721936.png "
-                  width={24}
-                  height={24}
-                  className="img-small fill-primary text-primary"
-                /> */}
+        <div class="-mx-3 mt-10 flex flex-wrap gap-y-4">
+          <div class="w-full px-3 2xsm:w-1/2">
+            <button
+              type="submit"
+              class="block w-full rounded border border-primary bg-primary p-3 text-center font-medium text-white transition hover:bg-opacity-90"
+            >
+              Save
+            </button>
+          </div>
+
+          <div class="w-full px-3 2xsm:w-1/2">
+            <button class="block w-full rounded border border-stroke bg-gray p-3 text-center font-medium text-black transition hover:border-meta-1 hover:bg-meta-1 hover:text-white dark:border-strokedark dark:bg-meta-4 dark:text-white dark:hover:border-meta-1 dark:hover:bg-meta-1">
+              Cancel
+            </button>
+          </div>
+        </div>
       </form>
-
-      <div class="-mx-3 mt-10 flex flex-wrap gap-y-4">
-        <div class="w-full px-3 2xsm:w-1/2">
-          <button
-            type="submit"
-            class="block w-full rounded border border-primary bg-primary p-3 text-center font-medium text-white transition hover:bg-opacity-90"
-          >
-            Save
-          </button>
-        </div>
-
-        <div class="w-full px-3 2xsm:w-1/2">
-          <button class="block w-full rounded border border-stroke bg-gray p-3 text-center font-medium text-black transition hover:border-meta-1 hover:bg-meta-1 hover:text-white dark:border-strokedark dark:bg-meta-4 dark:text-white dark:hover:border-meta-1 dark:hover:bg-meta-1">
-            Cancel
-          </button>
-        </div>
-      </div>
     </div>
   );
 };
