db7872d71fa6e06cfd5b48639ebfcd652b5366f7
Eyader Tsehayu <eyaderts@gmail.com>
2023-12-05 19:32:09 +0300

    add User model

---
+++ models/user.js
@@ -0,0 +1,42 @@
+import { Schema, model, models } from "mongoose";
+
+const UserSchema = new Schema({
+  userId: {
+    type: String,
+    unique: [true, "ID already exists!"],
+    required: [true, "Id is required!"],
+  },
+  firstname: {
+    type: String,
+    required: [true, "firstname is required"],
+  },
+  middlename: {
+    type: String,
+    required: [true, "middlename is required"],
+  },
+  lastname: {
+    type: String,
+    required: [true, "lastname is required"],
+  },
+  collegeId: {
+    type: String,
+  },
+  departmentId: {
+    type: String,
+  },
+  officename: {
+    type: String,
+  },
+  image: {
+    type: String,
+  },
+  year: {
+    type: String,
+  },
+  role: {
+    type: String,
+  },
+});
+
+const User = models.User || model("User", UserSchema);
+export default User;
