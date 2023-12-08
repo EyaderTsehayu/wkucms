ba626d32e46fef2ee8580355c2738f7f69fe4625
eyob-kefale <eyobkefale1@gmail.com>
2023-12-08 13:30:33 +0300

    fetch sudent data

--- app/admin/student/page.jsx
+++ app/admin/student/page.jsx
@@ -3,6 +3,8 @@ import Filter from "@/components/Admin/Filter";
 import AdminContainer from "@/components/Admin/AdminContainer";
 import { Metadata } from "next";
 import RegisterStudent from "@/components/Modals/RegisterStudent";
+import { useState } from "react";
+import { useEffect } from "react";
 // export const metadata = {
 //   title: "WKUCMS | Admin",
 //   description: "this a clearance management system for Wolkite University",
@@ -25,13 +27,26 @@ const collegeData = [
   // Add more office data here
 ];
 
+// const columns = [
+//   { field: "id", headerName: "ID", width: "100" },
+//   { field: "firstName", headerName: "First name", width: "160" },
+//   { field: "lastName", headerName: "Last name", width: "160" },
+//   { field: "college", headerName: "College", width: "160" },
+//   { field: "department", headerName: "Department", width: "160" },
+// ];
+
+
 const columns = [
-  { field: "id", headerName: "ID", width: "100" },
-  { field: "firstName", headerName: "First name", width: "160" },
-  { field: "lastName", headerName: "Last name", width: "160" },
-  { field: "college", headerName: "College", width: "160" },
-  { field: "department", headerName: "Department", width: "160" },
+  { field: "userId", headerName: "ID", width: "100" },
+  { field: "firstname", headerName: "First name", width: "160" },
+  { field: "middlename", headerName: "middle name", width: "160" },
+  { field: "lastname", headerName: "Last name", width: "160" },
+  { field: "collegeId", headerName: "College", width: "160" },
+  { field: "departmentId", headerName: "Department", width: "160" },
+  { field: "year", headerName: "year", width: "160" },
+  { field: "role", headerName: "Role", width: "160" },
 ];
+
 
 const rows = [
   {
@@ -99,13 +114,49 @@ const rows = [
   },
 ];
 
+
+
+
 const ManageStudent = () => {
+  const [userData, setUserData] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        // Fetch data from your API
+        const response = await fetch('http://localhost:3000/api/user/new/student'); // Update with your actual API endpoint
+        const data = await response.json();
+        const updatedData = data.map(user => ({ ...user, id: user._id }));
+        // Update state with the fetched data
+        setUserData(updatedData);
+        setLoading(false);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+        setError('Failed to fetch data');
+        setLoading(false);
+      }
+    };
+  
+    // Call the fetchData function when the component mounts
+    fetchData();
+  }, []); // The empty dependency array ensures that useEffect runs only once, similar to componentDidMount
+  
+  if (loading) {
+    <p>Loading...</p>;
+  }
+  
+  if (error) {
+   <p>{error}</p>;
+  }
+
+
   return (
     <>
       <div className="grid grid-cols-12 gap-4 md:gap-6 2xl:gap-4.5">
         <Filter officeData={officeData} collegeData={collegeData} />
 
-        <AdminContainer columns={columns} rows={rows} modal={RegisterStudent} />
+        <AdminContainer columns={columns} rows={userData} modal={RegisterStudent} />
       </div>
     </>
   );
