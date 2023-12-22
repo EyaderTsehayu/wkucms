6c2b7dfb62698ce3060f2c10bb3eb14345398cc0
Eyader Tsehayu <eyaderts@gmail.com>
2023-12-22 19:58:46 +0300

    add a model for student clearance request

---
+++ models/studentClearanceRequest.js
@@ -0,0 +1,22 @@
+import { Schema, model, models } from "mongoose";
+
+const StudentRequestSchema = new Schema({
+  userId: {
+    type: String,
+    required: [true, "Id is required!"],
+  },
+  reason: {
+    type: String,
+    required: [true, "Reason is required!"],
+  },
+  status: {
+    type: String,
+    required: [true, "Status is required!"],
+  },
+});
+
+const StudentClearnceReq =
+  models.StudentClearnceReq ||
+  model("StudentClearnceReq", StudentRequestSchema);
+
+export default StudentClearnceReq;
