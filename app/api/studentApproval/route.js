8a0f3e58b54a0dc236bd5d982f4460a51bbbbabf
Eyader Tsehayu <eyaderts@gmail.com>
2024-01-02 08:15:46 +0300

    fix error on steps difference

--- app/api/studentApproval/route.js
+++ app/api/studentApproval/route.js
@@ -1,23 +1,18 @@
-
 import { connectToDB } from "@/utils/database";
 import StudentClearnceReq from "@/models/studentClearanceRequest";
 import { useSession } from "next-auth/react";
 import { getServerSession } from "next-auth";
 import { authOptions } from "../auth/[...nextauth]/route";
 
-
 export const GET = async () => {
   const session = await getServerSession(authOptions);
-  const privilage=session?.user?.privilege;
-  
-   console.log("session from studentApproval ",session?.user?.privilege)
+  const privilage = session?.user?.privilege;
+
+  console.log("session from studentApproval ", session?.user?.privilege);
   try {
-
-   
     await connectToDB();
-    
-    const requests = await StudentClearnceReq.find({ status:privilage});
 
+    const requests = await StudentClearnceReq.find({ status: privilage });
 
     // Return a success response with the users data
     return new Response(JSON.stringify(requests), { status: 200 });
