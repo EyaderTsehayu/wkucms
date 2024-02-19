3c24ff9eafa7e7a72cc5ad15653831d54fcfdd6b
eyob-kefale <eyobkefale1@gmail.com>
2024-02-19 23:10:46 +0300

    approve without himself

--- app/api/staffApproval/route.js
+++ app/api/staffApproval/route.js
@@ -8,13 +8,14 @@ import { authOptions } from "../auth/[...nextauth]/route";
 export const GET = async () => {
   const session = await getServerSession(authOptions);
   const privilage=session?.user?.privilege;
+  const id = session?.user?.userId;
   console.log("privilageprivilage",privilage)
    console.log("session from studentApproval ",session?.user?.privilege)
   try {
 
     await connectToDB();
    
-    const requests = await StaffRequestSchema.find({ status:privilage});
+    const requests = await StaffRequestSchema.find({ status:privilage,userId: { $ne: id }});
 
 
     // Return a success response with the users data
