be10ee4d30bc9c011b832cf45989f755346cc00e
Eyader Tsehayu <eyaderts@gmail.com>
2024-03-02 21:19:49 +0300

    wrap layout with socket provider

--- app/user/layout.jsx
+++ app/user/layout.jsx
@@ -5,6 +5,7 @@ import Loader from "@/components/common/Loader";
 
 import Sidebar from "@/components/Sidebar/page";
 import Header from "@/components/Header/page";
+import { SocketProvider } from "@/context/SocketContext";
 
 export default function userLayout({ children }) {
   const [sidebarOpen, setSidebarOpen] = useState(false);
@@ -16,40 +17,38 @@ export default function userLayout({ children }) {
   }, []);
 
   return (
-    <html lang="en">
-      <body suppressHydrationWarning={true}>
-        <div className="dark:bg-boxdark-2 dark:text-bodydark">
-          {loading ? (
-            <Loader />
-          ) : (
-            <div className="flex h-screen overflow-hidden">
-              {/* <!-- ===== Sidebar Start ===== --> */}
-              <Sidebar
+    <SocketProvider>
+      <div className="dark:bg-boxdark-2 dark:text-bodydark">
+        {loading ? (
+          <Loader />
+        ) : (
+          <div className="flex h-screen overflow-hidden">
+            {/* <!-- ===== Sidebar Start ===== --> */}
+            <Sidebar
+              sidebarOpen={sidebarOpen}
+              setSidebarOpen={setSidebarOpen}
+            />
+            {/* <!-- ===== Sidebar End ===== --> */}
+
+            {/* <!-- ===== Content Area Start ===== --> */}
+            <div className="relative flex flex-1 flex-col overflow-y-auto overflow-x-hidden">
+              {/* <!-- ===== Header Start ===== --> */}
+              <Header
                 sidebarOpen={sidebarOpen}
                 setSidebarOpen={setSidebarOpen}
               />
-              {/* <!-- ===== Sidebar End ===== --> */}
+              {/* <!-- ===== Header End ===== --> */}
 
-              {/* <!-- ===== Content Area Start ===== --> */}
-              <div className="relative flex flex-1 flex-col overflow-y-auto overflow-x-hidden">
-                {/* <!-- ===== Header Start ===== --> */}
-                <Header
-                  sidebarOpen={sidebarOpen}
-                  setSidebarOpen={setSidebarOpen}
-                />
-                {/* <!-- ===== Header End ===== --> */}
-
-                {/* <!-- ===== Main Content Start ===== --> */}
-                <main>
-                  <div className="mx-auto max-w-screen-2xl  ">{children}</div>
-                </main>
-                {/* <!-- ===== Main Content End ===== --> */}
-              </div>
-              {/* <!-- ===== Content Area End ===== --> */}
+              {/* <!-- ===== Main Content Start ===== --> */}
+              <main>
+                <div className="mx-auto max-w-screen-2xl  ">{children}</div>
+              </main>
+              {/* <!-- ===== Main Content End ===== --> */}
             </div>
-          )}
-        </div>
-      </body>{" "}
-    </html>
+            {/* <!-- ===== Content Area End ===== --> */}
+          </div>
+        )}
+      </div>
+    </SocketProvider>
   );
 }
