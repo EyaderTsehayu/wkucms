e0ce8777ea95977ff1738834511b9927bfee1749
Eyob Kefale <97896057+eyob-kefale@users.noreply.github.com>
2024-03-02 21:18:11 +0300

    Update route.js

--- app/api/user/new/[id]/route.js
+++ app/api/user/new/[id]/route.js
@@ -1,35 +1,3 @@
-
-// import { connectToDB } from "@/utils/database";
-// import User from "@/models/user";
-
-
-// export const GET = async (request, { params }) => {
-//   try {
-//     // Get the user session
-//     // const session = await getServerSession();
-//     // console.log("session from studentApproval", session?.user?.privilege);
-
-//     await connectToDB();
-
-//     // Uncomment the following line if you want to use clearanceId from params
-//     // const clearanceId = params.id;
-
-//     // Or if you want to use clearanceId from request.query, uncomment the following line
-//     // const { clearanceId } = request.query;
-
-//     // Log the clearanceId
-//      console.log("staffUserId", params.id );
-
-//     const requests = await User.find({ userId: params.id });
-
-//     // Return a success response with the users data
-//     return new Response(JSON.stringify(requests), { status: 200 });
-//   } catch (error) {
-//     console.error("Error fetching requests:", error);
-//     // Return an error response
-//     return new Response("Failed to fetch requests", { status: 500 });
-//   }
-// };
 import { connectToDB } from "@/utils/database";
 import User from "@/models/user";
 
