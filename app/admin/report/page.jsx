d47e83ecf65f41b016d5f23b9224f2d27bb12374
Eyader Tsehayu <eyaderts@gmail.com>
2024-03-04 11:20:59 +0300

    start to implement reports ui

--- app/admin/report/page.jsx
+++ app/admin/report/page.jsx
@@ -1,7 +1,18 @@
+import ClearanceNums from "@/components/Report/ClearanceNums";
+import ReasonStaffReports from "@/components/Report/ReasonStaffReports";
+import ReasonStudentReports from "@/components/Report/ReasonStudentReports";
 import React from "react";
 
 const ManageReport = () => {
-  return <div>ManageReport</div>;
+  return (
+    <div>
+      <ClearanceNums />
+      <div class=" col-span-12 row-span-6 xl:col-span-6">
+        <ReasonStudentReports />
+        <ReasonStaffReports />
+      </div>
+    </div>
+  );
 };
 
 export default ManageReport;
