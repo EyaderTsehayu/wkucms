732b77000fb510771ca7193594ba9d3dade3e84d
Eyader Tsehayu <eyaderts@gmail.com>
2024-03-26 08:27:30 +0300

    fetch and pass correct reason for report

--- app/admin/report/page.jsx
+++ app/admin/report/page.jsx
@@ -1,15 +1,35 @@
+"use client";
 import ClearanceNums from "@/components/Report/ClearanceNums";
 import ReasonStaffReports from "@/components/Report/ReasonStaffReports";
 import ReasonStudentReports from "@/components/Report/ReasonStudentReports";
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const ManageReport = () => {
+  const [reasonReport, setReasonReport] = useState([]);
+
+  useEffect(() => {
+    const fetchReasonReports = async () => {
+      try {
+        const response = await fetch("/api/approvalHistory/reasonReport");
+        const data = await response.json();
+
+        if (response.ok) {
+          setReasonReport(data);
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchReasonReports();
+  }, []);
+
+  //console.log("mange report", reasonReport);
   return (
     <div>
-      <ClearanceNums />
+      <ClearanceNums approvedNum={reasonReport.length} />
       <div class=" col-span-12 row-span-6 xl:col-span-6">
-        <ReasonStudentReports />
-        <ReasonStaffReports />
+        <ReasonStudentReports reason={reasonReport} />
+        <ReasonStaffReports reason={reasonReport} />
       </div>
     </div>
   );
