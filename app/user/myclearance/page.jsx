9dc9f23702ee3f3cefc72ab0de1ef41f410f4fd0
Eyader Tsehayu <eyaderts@gmail.com>
2024-04-08 22:02:24 +0300

    fix fetching issues

--- app/user/myclearance/page.jsx
+++ app/user/myclearance/page.jsx
@@ -1,27 +1,26 @@
-"use client"
+"use client";
 
+import * as React from "react";
+import Box from "@mui/material/Box";
+import Tab from "@mui/material/Tab";
+import TabContext from "@mui/lab/TabContext";
+import TabList from "@mui/lab/TabList";
+import TabPanel from "@mui/lab/TabPanel";
+import PrintClearance from "@/components/User/PrintClearance";
+import Status from "@/components/User/Status";
+import RequestClearance from "@/components/User/RequestClearance";
+import History from "@/components/User/History";
 
-import * as React from 'react';
-import Box from '@mui/material/Box';
-import Tab from '@mui/material/Tab';
-import TabContext from '@mui/lab/TabContext';
-import TabList from '@mui/lab/TabList';
-import TabPanel from '@mui/lab/TabPanel';
-import PrintClearance from '@/components/User/PrintClearance';
-import Status from '@/components/User/Status';
-import RequestClearance from '@/components/User/RequestClearance';
-import History from '@/components/User/History';
-
-import RequestPageOutlinedIcon from '@mui/icons-material/RequestPageOutlined';
-import HistoryRoundedIcon from '@mui/icons-material/HistoryRounded';
-import AutorenewOutlinedIcon from '@mui/icons-material/AutorenewOutlined';
-import LocalPrintshopOutlinedIcon from '@mui/icons-material/LocalPrintshopOutlined';
+import RequestPageOutlinedIcon from "@mui/icons-material/RequestPageOutlined";
+import HistoryRoundedIcon from "@mui/icons-material/HistoryRounded";
+import AutorenewOutlinedIcon from "@mui/icons-material/AutorenewOutlined";
+import LocalPrintshopOutlinedIcon from "@mui/icons-material/LocalPrintshopOutlined";
 
 import { useState } from "react";
 import useSWR from "swr";
 
 const iconStyle = {
-  marginRight: '8px', // Adjust the margin as needed
+  marginRight: "8px", // Adjust the margin as needed
 };
 const fetcher = async (url) => {
   const response = await fetch(url);
@@ -35,13 +34,11 @@ const fetcher = async (url) => {
 };
 
 export default function LabTabs() {
-
- 
   // var index="1"
   // if(!userData[0].status){
   //   index="2"
   // }
-  const [value, setValue] =useState("3");
+  const [value, setValue] = useState("3");
 
   const handleChange = (event, newValue) => {
     setValue(newValue);
@@ -55,7 +52,7 @@ export default function LabTabs() {
     {
       initialData: a,
       revalidateOnFocus: false,
-      // refreshInterval: 2000, // Set the refresh interval in milliseconds (e.g., 10000 for 10 seconds)
+      refreshInterval: 2000, // Set the refresh interval in milliseconds (e.g., 10000 for 10 seconds)
     }
   );
   console.log("session from approval ad ", userData);
@@ -69,42 +66,65 @@ export default function LabTabs() {
     return <p>Failed to fetch data</p>;
   }
 
-
   return (
     <div className="bg-white sm:px-14 dark:bg-black dark:border-black">
-      <h1 className="pt-8 pb-5 pl-4 font-extrabold text-4xl text-primary dark:text-white">My Clearance</h1>
-      <Box sx={{ typography: 'body1', backgroundColor: "white", minHeight: "100vh" }} className="dark:border-black dark:bg-black" >
-        <TabContext value={value} >
-          <Box sx={{ borderBottom: 1, borderColor: 'divider' }} className="pt-2 px-4 dark:border-body">
-            <TabList indicatorColor="primary" textColor='#AEB7C0' onChange={handleChange} aria-label="lab API tabs example" >
+      <h1 className="pt-8 pb-5 pl-4 font-extrabold text-4xl text-primary dark:text-white">
+        My Clearance
+      </h1>
+      <Box
+        sx={{
+          typography: "body1",
+          backgroundColor: "white",
+          minHeight: "100vh",
+        }}
+        className="dark:border-black dark:bg-black"
+      >
+        <TabContext value={value}>
+          <Box
+            sx={{ borderBottom: 1, borderColor: "divider" }}
+            className="pt-2 px-4 dark:border-body"
+          >
+            <TabList
+              indicatorColor="primary"
+              textColor="#AEB7C0"
+              onChange={handleChange}
+              aria-label="lab API tabs example"
+            >
               {!userData[0]?.status && (
+                <Tab
+                  sx={{
+                    textTransform: "none",
+                    color: "black",
+                    fontSize: "1rem",
+                  }}
+                  className="dark:text-white"
+                  value="1"
+                  label={
+                    <div className="flex">
+                      <RequestPageOutlinedIcon className="mr-2" /> Request
+                      Clearance
+                    </div>
+                  }
+                />
+              )}
               <Tab
-                sx={{ textTransform: 'none', color: 'black', fontSize: '1rem' }}
-                className='dark:text-white'
-                value='1'
-                label={
-                  <div className='flex'>
-                    <RequestPageOutlinedIcon className='mr-2' /> Request Clearance
-                  </div>
-                }
-              />
-              )} 
-              <Tab sx={{ textTransform: 'none', color: "black", fontSize: "1rem" }}
-                className=' dark:text-white  '
+                sx={{ textTransform: "none", color: "black", fontSize: "1rem" }}
+                className=" dark:text-white  "
                 value="3"
                 label={
                   <div className="flex">
-                    <AutorenewOutlinedIcon className='mr-2' /> Status
+                    <AutorenewOutlinedIcon className="mr-2" /> Status
                   </div>
                 }
               />
 
-              <Tab sx={{ textTransform: 'none', color: "black", fontSize: "1rem" }}
-                className=' dark:text-white  '
+              <Tab
+                sx={{ textTransform: "none", color: "black", fontSize: "1rem" }}
+                className=" dark:text-white  "
                 value="2"
                 label={
                   <div className="flex">
-                    <AutorenewOutlinedIcon className='mr-2' /> History
+                    <AutorenewOutlinedIcon className="mr-2" /> History
                   </div>
                 }
               />
@@ -134,9 +154,6 @@ export default function LabTabs() {
               <PrintClearance />
             </TabPanel> */}
           </>
-
-
-
         </TabContext>
       </Box>
     </div>
--- app/user/staffApproval/page.jsx
+++ app/user/staffApproval/page.jsx
@@ -7,138 +7,145 @@ import RegisterStaff from "@/components/Modals/RegisterStaff";
 import useSWR from "swr";
 
 const columns = [
-    { field: "userId", headerName: "ID", width: "100" },
-    { field: "firstname", headerName: "First name", width: "160" },
-    { field: "middlename", headerName: "Last name", width: "160" },
-    { field: "reason", headerName: "Reason", width: "160" },
-    { field: "status", headerName: "Status", width: "160" },
+  { field: "userId", headerName: "ID", width: "100" },
+  { field: "firstname", headerName: "First name", width: "160" },
+  { field: "middlename", headerName: "Last name", width: "160" },
+  { field: "reason", headerName: "Reason", width: "160" },
+  { field: "status", headerName: "Status", width: "160" },
 ];
 
 const rows = [
-    {
-        id: 1,
-        lastName: "Snow",
-        firstName: "Jon",
-        department: "Seng",
-        officeName: "Library",
-    },
-    {
-        id: 2,
-        lastName: "Lannister",
-        firstName: "Cersei",
-        department: "Seng",
-        officeName: "Cafteria",
-    },
-    {
-        id: 3,
-        lastName: "Lannister",
-        firstName: "Jaime",
-        department: "Seng",
-        officeName: "dpt Head office",
-    },
-    {
-        id: 4,
-        lastName: "Stark",
-        firstName: "Arya",
-        department: "Seng",
-        officeName: "Collage dean",
-    },
-    {
-        id: 5,
-        lastName: "Targaryen",
-        firstName: "Daenerys",
-        department: "Seng",
-        officeName: "Dormitory",
-    },
-    {
-        id: 6,
-        lastName: "Melisandre",
-        firstName: "Drunk",
-        department: "Seng",
-        officeName: "Sport and Recreational",
-    },
-    {
-        id: 7,
-        lastName: "Clifford",
-        firstName: "Ferrara",
-        department: "Seng",
-        officeName: "CCI",
-    },
-    {
-        id: 8,
-        lastName: "Frances",
-        firstName: "Rossini",
-        department: "Seng",
-        officeName: "CCI",
-    },
-    {
-        id: 9,
-        lastName: "Roxie",
-        firstName: "Harvey",
-        department: "Seng",
-        officeName: "CCI",
-    },
+  {
+    id: 1,
+    lastName: "Snow",
+    firstName: "Jon",
+    department: "Seng",
+    officeName: "Library",
+  },
+  {
+    id: 2,
+    lastName: "Lannister",
+    firstName: "Cersei",
+    department: "Seng",
+    officeName: "Cafteria",
+  },
+  {
+    id: 3,
+    lastName: "Lannister",
+    firstName: "Jaime",
+    department: "Seng",
+    officeName: "dpt Head office",
+  },
+  {
+    id: 4,
+    lastName: "Stark",
+    firstName: "Arya",
+    department: "Seng",
+    officeName: "Collage dean",
+  },
+  {
+    id: 5,
+    lastName: "Targaryen",
+    firstName: "Daenerys",
+    department: "Seng",
+    officeName: "Dormitory",
+  },
+  {
+    id: 6,
+    lastName: "Melisandre",
+    firstName: "Drunk",
+    department: "Seng",
+    officeName: "Sport and Recreational",
+  },
+  {
+    id: 7,
+    lastName: "Clifford",
+    firstName: "Ferrara",
+    department: "Seng",
+    officeName: "CCI",
+  },
+  {
+    id: 8,
+    lastName: "Frances",
+    firstName: "Rossini",
+    department: "Seng",
+    officeName: "CCI",
+  },
+  {
+    id: 9,
+    lastName: "Roxie",
+    firstName: "Harvey",
+    department: "Seng",
+    officeName: "CCI",
+  },
 ];
 
 const fetcher = async (url) => {
-    const response = await fetch(url);
-    const data = await response.json();
-    const updatedData = data.map(user => ({ ...user, id: user._id, roleId: user._id }));
-    return updatedData;
-  };
+  const response = await fetch(url);
+  const data = await response.json();
+  const updatedData = data.map((user) => ({
+    ...user,
+    id: user._id,
+    roleId: user._id,
+  }));
+  return updatedData;
+};
 const ApproveStaff = () => {
-    const [searchTerm, setSearchTerm] = useState("");
-        // Use SWR to fetch and cache data with automatic refresh every 10 seconds
-        const { data: userData, error } = useSWR('http://localhost:3000/api/staffApproval', fetcher, {
-          initialData: rows,
-          revalidateOnFocus: false,
-        //   refreshInterval: 2000, // Set the refresh interval in milliseconds (e.g., 10000 for 10 seconds)
-        });
-        console.log("session from approval ad ",userData)
-        // Handle loading and fetch errors
-        if (!userData && !error) {
-          return <p>Loading...</p>;
-        }
-      
-        if (error) {
-          console.error('Error fetching data:', error);
-          return <p>Failed to fetch data</p>;
-        }
-    
+  const [searchTerm, setSearchTerm] = useState("");
+  // Use SWR to fetch and cache data with automatic refresh every 10 seconds
+  const { data: userData, error } = useSWR(
+    "http://localhost:3000/api/staffApproval",
+    fetcher,
+    {
+      initialData: rows,
+      revalidateOnFocus: false,
+      refreshInterval: 2000, // Set the refresh interval in milliseconds (e.g., 10000 for 10 seconds)
+    }
+  );
+  console.log("session from approval ad ", userData);
+  // Handle loading and fetch errors
+  if (!userData && !error) {
+    return <p>Loading...</p>;
+  }
 
+  if (error) {
+    console.error("Error fetching data:", error);
+    return <p>Failed to fetch data</p>;
+  }
 
-  
-    const filteredInfo = userData.filter((info) =>
-        info.reason.toLowerCase().includes(searchTerm.toLowerCase())
-    );
-    //const filteredInfo = rows.filter(info => info.id.includes(searchTerm));
+  const filteredInfo = userData.filter((info) =>
+    info.reason.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+  //const filteredInfo = rows.filter(info => info.id.includes(searchTerm));
 
-    const handleSearch = (event) => {
-        setSearchTerm(event.target.value);
-    };
+  const handleSearch = (event) => {
+    setSearchTerm(event.target.value);
+  };
 
-    return (
-        <div className="bg-white sm:px-14 dark:bg-black dark:border-black">
-            <h1 className="pt-8 pb-5 pl-4 font-extrabold text-4xl text-primary dark:text-white">Staff Approval</h1>
-            <div className="pt-2 px-4  ">
-                <input
-                    type="text"
-                    placeholder="Search office requirement here"
-                     value={searchTerm}
-                     onChange={handleSearch}
-                    className=" w-full sm:hidden pt-4 pb-3 px-3 py-4 mb-7  rounded-md  border border-stroke bg-gray  text-black focus:border-primary focus-visible:outline-none dark:border-strokedark dark:bg-meta-4 dark:text-white dark:focus:border-primary "
-                />
+  return (
+    <div className="bg-white sm:px-14 dark:bg-black dark:border-black">
+      <h1 className="pt-8 pb-5 pl-4 font-extrabold text-4xl text-primary dark:text-white">
+        Staff Approval
+      </h1>
+      <div className="pt-2 px-4  ">
+        <input
+          type="text"
+          placeholder="Search office requirement here"
+          value={searchTerm}
+          onChange={handleSearch}
+          className=" w-full sm:hidden pt-4 pb-3 px-3 py-4 mb-7  rounded-md  border border-stroke bg-gray  text-black focus:border-primary focus-visible:outline-none dark:border-strokedark dark:bg-meta-4 dark:text-white dark:focus:border-primary "
+        />
 
-                <div className=" grid grid-cols-12 ">
-                    <UserContainer
-                        columns={columns}
-                        rows={filteredInfo}
-                        modal={RegisterStaff}
-                    />
-                </div>
-            </div>
+        <div className=" grid grid-cols-12 ">
+          <UserContainer
+            columns={columns}
+            rows={filteredInfo}
+            modal={RegisterStaff}
+          />
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default ApproveStaff;
