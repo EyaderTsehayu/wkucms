1602901eef3e582f9f8333af7b5e3c41b1d24c72
Eyader Tsehayu <eyaderts@gmail.com>
2024-04-18 13:18:31 +0300

    fetch the requested clearances

--- app/user/studentApproval/page.jsx
+++ app/user/studentApproval/page.jsx
@@ -1,15 +1,26 @@
 "use client";
 import Filter from "@/components/Admin/Filter";
 import UserContainer from "@/components/User/UserContainer/UserContainer";
-import { Metadata } from "next";
 import RejectionMessageBox from "@/components/Modals/RejectionMessageBox";
 import useSWR from "swr";
-import { useState } from "react";
-// export const metadata = {
-//   title: "WKUCMS | Admin",
-//   description: "this a clearance management system for Wolkite University",
-//   // other metadata
-// };
+import { useState, useEffect } from "react";
+import { useSession } from "next-auth/react";
+const stages = {
+  Head: ["College Dean"],
+  "College Dean": [
+    "Dormitory",
+    "Cafteria",
+    "Sport And Recreation",
+    "College Book Store",
+  ],
+  Dormitory: ["Dean Of Student"],
+  Cafteria: ["Dean Of Student"],
+  "Sport And Recreation": ["Dean Of Student"],
+  "College Book Store": ["Library Chief"],
+  "Dean Of Student": ["Registrar"],
+  "Library Chief": ["Registrar"],
+  Registrar: ["APPROVED"],
+};
 
 // Replace this with your actual office data
 const officeData = [
@@ -35,72 +46,6 @@ const columns = [
   { field: "status", headerName: "Status", width: "160" },
 ];
 
-const rows = [
-  {
-    id: 1,
-    lastName: "Snow",
-    firstName: "Jon",
-    department: "Seng",
-    college: "CCI",
-  },
-  {
-    id: 2,
-    lastName: "Lannister",
-    firstName: "Cersei",
-    department: "Seng",
-    college: "CCI",
-  },
-  {
-    id: 3,
-    lastName: "Lannister",
-    firstName: "Jaime",
-    department: "Seng",
-    college: "CCI",
-  },
-  {
-    id: 4,
-    lastName: "Stark",
-    firstName: "Arya",
-    department: "Seng",
-    college: "CCI",
-  },
-  {
-    id: 5,
-    lastName: "Targaryen",
-    firstName: "Daenerys",
-    department: "Seng",
-    college: "CCI",
-  },
-  {
-    id: 6,
-    lastName: "Melisandre",
-    firstName: "Drunk",
-    department: "Seng",
-    college: "CCI",
-  },
-  {
-    id: 7,
-    lastName: "Clifford",
-    firstName: "Ferrara",
-    department: "Seng",
-    college: "CCI",
-  },
-  {
-    id: 8,
-    lastName: "Frances",
-    firstName: "Rossini",
-    department: "Seng",
-    college: "CCI",
-  },
-  {
-    id: 9,
-    lastName: "Roxie",
-    firstName: "Harvey",
-    department: "Seng",
-    college: "CCI",
-  },
-];
-
 const fetcher = async (url) => {
   const response = await fetch(url);
   const data = await response.json();
@@ -115,18 +60,45 @@ const fetcher = async (url) => {
 const ApproveStudent = () => {
   // Use useSWR to fetch data
   const [searchTerm, setSearchTerm] = useState("");
-
+  const { data: session } = useSession();
+  const [filteredData, setFilteredData] = useState([]);
+  const previlage = session?.user?.privilege;
+  console.log("previlage inside student approval", previlage);
   // Use SWR to fetch and cache data with automatic refresh every 10 seconds
   const { data: userData, error } = useSWR(
     "http://localhost:3000/api/studentApproval",
     fetcher,
     {
-      initialData: rows,
       revalidateOnFocus: false,
-      refreshInterval: 2000, // Set the refresh interval in milliseconds (e.g., 10000 for 10 seconds)
+      refreshInterval: 2000,
     }
   );
-  console.log("session from approval ad ", userData);
+  // console.log("data from student request", userData);
+  useEffect(() => {
+    if (userData && previlage && previlage !== "Head") {
+      const filtered = userData.filter((request) => {
+        const approvalRoles = request.approvals.map(
+          (approval) => approval.role
+        );
+
+        const stageKeys = Object.keys(stages).filter((key) =>
+          stages[key].includes(previlage)
+        );
+
+        // Check if the request has approvals for all stageKeys
+        const hasAllApprovals = stageKeys.every((key) =>
+          approvalRoles.includes(key)
+        );
+
+        // Check if the previlage is not in the rejections array
+        const notRejected = !request.rejections.includes(previlage);
+
+        return hasAllApprovals && notRejected;
+      });
+      setFilteredData(filtered);
+    }
+  }, [userData, previlage]);
+
   // Handle loading and fetch errors
   if (!userData && !error) {
     return <p>Loading...</p>;
@@ -147,7 +119,7 @@ const ApproveStudent = () => {
 
         <UserContainer
           columns={columns}
-          rows={userData}
+          rows={previlage !== "Head" ? filteredData : userData}
           modal={RejectionMessageBox}
         />
       </div>
