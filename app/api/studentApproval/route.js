6599b7443daa6f525d160f6d7d3cf749d5e3a6bc
Eyader Tsehayu <eyaderts@gmail.com>
2024-04-18 13:16:25 +0300

    fetch with new way of approval

--- app/api/studentApproval/route.js
+++ app/api/studentApproval/route.js
@@ -11,28 +11,35 @@ export const GET = async () => {
   const collegeName = session?.user?.collegeName;
   const departmentName = session?.user?.departmentName;
   const userId = session?.user?.userId;
-  console.log("session from studentApproval ", collegeName, 'rtyey', departmentName);
+
   try {
     await connectToDB();
 
-
-
     const fetchStudent = await User.find({ userId: userId });
-    console.log("fetchStudent for dorm", privilege);
+    // console.log("fetchStudent for dorm", privilege);
 
-
-    const requests = collegeName && departmentName
-      ? await StudentClearnceReq.find({ status: privilege, collegeName: collegeName, departmentName: departmentName })
-      : collegeName
-        ? await StudentClearnceReq.find({ status: privilege, collegeName: collegeName })
+    // find all requests where the previlage is included inside the status array which is one field in the schema
+    const requests =
+      collegeName && departmentName
+        ? await StudentClearnceReq.find({
+            status: privilege,
+            collegeName: collegeName,
+            departmentName: departmentName,
+          })
+        : collegeName
+        ? await StudentClearnceReq.find({
+            status: privilege,
+            collegeName: collegeName,
+          })
         : privilege == "Dormitory"
-          ? await StudentClearnceReq.find({ status: privilege, blockNo: fetchStudent[0].blockNo })
-          : await StudentClearnceReq.find({ status: privilege });
-   
-   
+        ? await StudentClearnceReq.find({
+            status: privilege,
+            blockNo: fetchStudent[0].blockNo,
+          })
+        : await StudentClearnceReq.find({ status: privilege });
+
     // Return a success response with the users data
     return new Response(JSON.stringify(requests), { status: 200 });
-
   } catch (error) {
     console.error("Error fetching requests:", error);
 
