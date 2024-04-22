50f358938cc7529564dd95f5e8152f8a4e164f77
Eyader Tsehayu <eyaderts@gmail.com>
2024-04-22 21:01:14 +0300

    add main styling for all

--- app/layout.js
+++ app/layout.js
@@ -1,18 +1,17 @@
 import "./globals.css";
-import { Montserrat, Poppins } from "next/font/google";
+import { Poppins } from "next/font/google";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { AuthProvider } from "./provider";
 
-const montserrat = Montserrat({
-  variable: "--montserrat-font",
-  subsets: ["latin"],
-});
-// const poppins = Poppins({
-//   variable: "--poppins-font",
-//   weight: ["100", "300", "400", "500", "700", "900"],
+// const montserrat = Montserrat({
+//   variable: "--montserrat-font",
 //   subsets: ["latin"],
 // });
+const poppins = Poppins({
+  weight: "400",
+  subsets: ["latin"],
+});
 export const metadata = {
   title: "WKUCMS",
   description:
@@ -24,7 +23,7 @@ export default function RootLayout({ children }) {
     // <html lang="en" className={`${montserrat.variable} ${poppins.variable} `}>
     <html lang="en">
       <AuthProvider>
-        <body className={montserrat.className}>
+        <body className={poppins.className}>
           {children}{" "}
           <ToastContainer
             position="top-right"
