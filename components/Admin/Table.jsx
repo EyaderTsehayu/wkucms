8a9818402d897b1a86314575b6d2f2376b57fb47
Eyader Tsehayu <eyaderts@gmail.com>
2024-04-22 21:00:34 +0300

    change fonts and styles of table

--- components/Admin/Table.jsx
+++ components/Admin/Table.jsx
@@ -1,80 +1,49 @@
-// import { Box } from "@mui/material";
-// import { DataGrid } from "@mui/x-data-grid";
-// import { useState, useMemo, useEffect } from "react";
+import { Box, CssBaseline, ThemeProvider, createTheme } from "@mui/material";
 
-// const Table = ({ columns, rows, setSelectedRows, handleApproveAll,clickableColumns }) => {
-//   const [rowSelectionModel, setRowSelectionModel] = useState([]);
-
-//   // const selectedRowsData = rows.filter((row) => row._id == rowSelectionModel);
-
-//   const selectedRowsData = useMemo(
-//     () => rows.filter((row) => rowSelectionModel.includes(row._id)),
-//     [rowSelectionModel, rows]
-//   );
-
-//   useEffect(() => {
-//     setSelectedRows(selectedRowsData);
-//   }, [selectedRowsData, setSelectedRows]);
-
-//   // console.log("Selected Rows Data:", selectedRowsData);
-
-
-//   return (
-//     <Box
-//       style={{ height: 520, width: "100%" }}
-//       sx={{
-//         "& .MuiDataGrid-footerContainer": {
-//           borderTop: "none",
-//           backgroundColor: "#34e1eb",
-//         },
-//         "& .MuiCheckbox-root": {
-//           color: "#64748B",
-//         },
-//       }}
-//       className="dark:bg-boxdark-2"
-//     >
-//       <DataGrid
-//         rows={rows}
-//         columns={columns}
-//         initialState={{
-//           pagination: {
-//             paginationModel: { page: 0, pageSize: 10 },
-//           },
-//         }}
-//         pageSizeOptions={[5, 10]}
-//         checkboxSelection
-//         className="dark:bg-gray-800 dark:text-gray "
-//         onRowSelectionModelChange={(newRowSelectionModel) => {
-//           setRowSelectionModel(newRowSelectionModel);
-//         }}
-//         rowSelectionModel={rowSelectionModel}
-//       />
-//     </Box>
-//   );
-// };
-
-// export default Table;
-
-
-
-
-import { Box } from "@mui/material";
-import { DataGrid } from "@mui/x-data-grid";
+import {
+  DataGrid,
+  GridToolbarContainer,
+  GridToolbarColumnsButton,
+  GridToolbarFilterButton,
+  GridToolbarExport,
+  GridToolbarDensitySelector,
+} from "@mui/x-data-grid";
 import { useState, useMemo, useEffect } from "react";
-import InsertPhotoIcon from '@mui/icons-material/InsertPhoto';
-import OpenInNewIcon from '@mui/icons-material/OpenInNew';
+import InsertPhotoIcon from "@mui/icons-material/InsertPhoto";
+import OpenInNewIcon from "@mui/icons-material/OpenInNew";
 
-const Table = ({ columns, rows, setSelectedRows, handleApproveAll, clickableColumns }) => {
+const Table = ({
+  columns,
+  rows,
+  setSelectedRows,
+  handleApproveAll,
+  clickableColumns,
+}) => {
   const [rowSelectionModel, setRowSelectionModel] = useState([]);
   const [filteredRows, setFilteredRows] = useState(rows);
+
+  const theme = createTheme({
+    typography: {
+      fontFamily: "Satoshi",
+    },
+    components: {
+      MuiCssBaseline: {
+        styleOverrides: `
+       @font-face {
+  font-family: "Satoshi";
+  src: url("../../app/fonts/Satoshi-Regular.woff2") format("woff2")
+  font-weight: 400;
+  font-display: swap;
+  font-style: normal;
+}
+      `,
+      },
+    },
+  });
 
   if (!clickableColumns) {
     clickableColumns = [-1];
   }
-
-
-
-
 
   const selectedRowsData = useMemo(
     () => rows.filter((row) => rowSelectionModel.includes(row._id)),
@@ -96,52 +65,74 @@ const Table = ({ columns, rows, setSelectedRows, handleApproveAll, clickableColu
     newWindow.document.write(`<img src="${imageData}" />`);
   };
 
+  function CustomToolbar() {
+    return (
+      <GridToolbarContainer>
+        <GridToolbarColumnsButton />
+        <GridToolbarFilterButton />
+        <GridToolbarExport />
+      </GridToolbarContainer>
+    );
+  }
 
   return (
-    <Box
-      style={{ height: 520, width: "100%" }}
-      sx={{
-        "& .MuiDataGrid-footerContainer": {
-          borderTop: "none",
-          backgroundColor: "#34e1eb",
-        },
-        "& .MuiCheckbox-root": {
-          color: "#64748B",
-        },
-      }}
-      className="dark:bg-boxdark-2"
-    >
-      <DataGrid
-        rows={rows}
-        columns={columns.map((column, index) => ({
-          ...column,
-          renderCell: (params) => (
-            <div
-           
-              style={{ cursor: clickableColumns.includes(index) ? 'pointer' : 'auto',paddingLeft:clickableColumns.includes(index) ? '20px' : '0px'}}
-              onClick={() => handleClick(params, params.id)}
-            >
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Box
+        style={{ height: 520, width: "100%" }}
+        sx={{
+          "& .MuiDataGrid-footerContainer": {
+            borderTop: "none",
+            color: "#5e35b1",
 
-              {(clickableColumns.includes(index) && params.value)
-                ? <OpenInNewIcon />
-                : params.value}
-            </div>
-          ),
-        }))}
-        initialState={{
-          pagination: {
-            paginationModel: { page: 0, pageSize: 10 },
+            //backgroundColor: "#34e1eb",
+          },
+          "& .MuiCheckbox-root": {
+            color: "#5f62fa",
           },
         }}
-        pageSizeOptions={[5, 10]}
-        checkboxSelection
-        className="dark:bg-gray-800 dark:text-gray "
-        onRowSelectionModelChange={(newRowSelectionModel) => {
-          setRowSelectionModel(newRowSelectionModel);
-        }}
-        rowSelectionModel={rowSelectionModel}
-      />
-    </Box>
+      >
+        <DataGrid
+          rows={rows}
+          columns={columns.map((column, index) => ({
+            ...column,
+            renderCell: (params) => (
+              <div
+                style={{
+                  cursor: clickableColumns.includes(index) ? "pointer" : "auto",
+                  paddingLeft: clickableColumns.includes(index)
+                    ? "20px"
+                    : "0px",
+                }}
+                onClick={() => handleClick(params, params.id)}
+              >
+                {clickableColumns.includes(index) && params.value ? (
+                  <OpenInNewIcon />
+                ) : (
+                  params.value
+                )}
+              </div>
+            ),
+          }))}
+          initialState={{
+            pagination: {
+              paginationModel: { page: 0, pageSize: 10 },
+            },
+          }}
+          pageSizeOptions={[5, 10]}
+          checkboxSelection
+          className="dark:bg-gray-800   dark:text-gray "
+          onRowSelectionModelChange={(newRowSelectionModel) => {
+            setRowSelectionModel(newRowSelectionModel);
+          }}
+          slots={{
+            toolbar: CustomToolbar,
+          }}
+          rowSelectionModel={rowSelectionModel}
+          sx={{ fontFamily: "Satoshi", fontSize: "15px", color: "#5e35b1" }}
+        />
+      </Box>
+    </ThemeProvider>
   );
 };
 
