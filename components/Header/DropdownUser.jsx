fc1ebac6eed33063dbdec2723c1b5b9c65ad3302
Eyader Tsehayu <eyaderts@gmail.com>
2024-04-27 21:13:53 +0300

    removing comments

--- components/Header/DropdownUser.jsx
+++ components/Header/DropdownUser.jsx
@@ -11,21 +11,19 @@ const DropdownUser = () => {
   const dropdown = useRef(null);
 
   const { data: session } = useSession();
-  const userId = session?.user?.userId
+  const userId = session?.user?.userId;
   const Id = session?.user.id;
   const Role = session?.user.role;
-  console.log("session user from dropdown", Role)
-  //fetch user data
-  
 
-  
+  //fetch user data
+
   useEffect(() => {
-    console.log("oooooo",Id);
+    console.log("oooooo", Id);
     const fetchUserData = async () => {
       try {
         const response = await fetch(`/api/user/new/${Id}`);
         if (!response.ok) {
-          throw new Error('Network response was not ok');
+          throw new Error("Network response was not ok");
         }
         const fetchedData = await response.json();
         // const updatedData = fetchedData.map((user) => ({
@@ -39,7 +37,7 @@ const DropdownUser = () => {
     };
 
     fetchUserData();
-  }, [Id,userData]);
+  }, [Id, userData]);
   // close on click outside
   useEffect(() => {
     const clickHandler = ({ target }) => {
@@ -102,7 +100,6 @@ const DropdownUser = () => {
           )}
         </span>
 
-
         <svg
           className="hidden fill-current sm:block"
           width="12"
@@ -125,13 +122,14 @@ const DropdownUser = () => {
         ref={dropdown}
         onFocus={() => setDropdownOpen(true)}
         onBlur={() => setDropdownOpen(false)}
-        className={`absolute right-0 mt-4 flex w-62.5 flex-col rounded-sm border border-stroke bg-white shadow-default dark:border-strokedark dark:bg-boxdark ${dropdownOpen === true ? "block" : "hidden"
-          }`}
+        className={`absolute right-0 mt-4 flex w-62.5 flex-col rounded-sm border border-stroke bg-white shadow-default dark:border-strokedark dark:bg-boxdark ${
+          dropdownOpen === true ? "block" : "hidden"
+        }`}
       >
         <ul className="flex flex-col gap-5 border-b border-stroke px-6 py-7.5 dark:border-strokedark">
           <li>
             <Link
-              href={Role=="ADMIN"?"/admin/settings":"/user/settings"}
+              href={Role == "ADMIN" ? "/admin/settings" : "/user/settings"}
               className="flex items-center gap-3.5 text-sm font-medium duration-300 ease-in-out hover:text-primary lg:text-base"
             >
               <svg
