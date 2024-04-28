8c4fec4e7d5170093b95f8498095a1c5a1d790be
Eyader Tsehayu <eyaderts@gmail.com>
2024-04-29 00:05:46 +0300

    rename the folder name for routing

--- app/user/ApprovedUsers/page.jsx
+++ app/user/ApprovedUsers/page.jsx
@@ -1,106 +1,17 @@
 "use client";
-import Filter from "@/components/Admin/Filter";
 import UserContainer from "@/components/User/UserContainer/UserContainer";
-import { Metadata } from "next";
-import RejectionMessageBox from "@/components/Modals/RejectionMessageBox";
 import useSWR from "swr";
 import { useState } from "react";
-// export const metadata = {
-//   title: "WKUCMS | Admin",
-//   description: "this a clearance management system for Wolkite University",
-//   // other metadata
-// };
-
-// Replace this with your actual office data
-const officeData = [
-  { id: 1, name: "Office 1" },
-  { id: 2, name: "Office 2" },
-  { id: 3, name: "Office 3" },
-  // Add more office data here
-];
-const collegeData = [
-  { id: 1, name: "College of Computing and Informatics" },
-  { id: 2, name: "Engineering" },
-  { id: 3, name: "Social sciences and Humanities" },
-  { id: 4, name: "College of behavioral science" },
-
-  // Add more office data here
-];
+import Breadcrumb from "@/components/Breadcrumb/breadcrumb";
 
 const columns = [
   { field: "userId", headerName: "ID", width: "100" },
-  { field: "firstname", headerName: "First name", width: "160" },
-  { field: "middlename", headerName: "Last name", width: "160" },
+  { field: "firstname", headerName: "First Name", width: "160" },
+  { field: "middlename", headerName: "Last Name", width: "160" },
   { field: "reason", headerName: "Reason", width: "160" },
   { field: "status", headerName: "Status", width: "160" },
-  { field: "dateRequested", headerName: "dateRequested", width: "160" },
-  { field: "dateApproved", headerName: "dateApproved", width: "160" },
-];
-
-const rows = [
-  {
-    id: 1,
-    lastName: "Snow",
-    firstName: "Jon",
-    department: "Seng",
-    college: "CCI",
-  },
-  {
-    id: 2,
-    lastName: "Lannister",
-    firstName: "Cersei",
-    department: "Seng",
-    college: "CCI",
-  },
-  {
-    id: 3,
-    lastName: "Lannister",
-    firstName: "Jaime",
-    department: "Seng",
-    college: "CCI",
-  },
-  {
-    id: 4,
-    lastName: "Stark",
-    firstName: "Arya",
-    department: "Seng",
-    college: "CCI",
-  },
-  {
-    id: 5,
-    lastName: "Targaryen",
-    firstName: "Daenerys",
-    department: "Seng",
-    college: "CCI",
-  },
-  {
-    id: 6,
-    lastName: "Melisandre",
-    firstName: "Drunk",
-    department: "Seng",
-    college: "CCI",
-  },
-  {
-    id: 7,
-    lastName: "Clifford",
-    firstName: "Ferrara",
-    department: "Seng",
-    college: "CCI",
-  },
-  {
-    id: 8,
-    lastName: "Frances",
-    firstName: "Rossini",
-    department: "Seng",
-    college: "CCI",
-  },
-  {
-    id: 9,
-    lastName: "Roxie",
-    firstName: "Harvey",
-    department: "Seng",
-    college: "CCI",
-  },
+  { field: "dateRequested", headerName: "Date Requested", width: "160" },
+  { field: "dateApproved", headerName: "Date Approved", width: "160" },
 ];
 
 const fetcher = async (url) => {
@@ -123,16 +34,11 @@ const ApproveStudent = () => {
     "/api/approvalHistory/approvedHistory",
     fetcher,
     {
-      initialData: rows,
+      // initialData: rows,
       revalidateOnFocus: false,
       refreshInterval: 2000, // Set the refresh interval in milliseconds (e.g., 10000 for 10 seconds)
     }
   );
-
- 
-  
-  
-
 
   console.log("session from approval ad ", userData);
   // Handle loading and fetch errors
@@ -146,18 +52,16 @@ const ApproveStudent = () => {
   }
 
   return (
-    <div className="bg-white sm:px-14 dark:bg-black dark:border-black">
-      <h1 className="pt-8 pb-5 pl-4 font-extrabold text-4xl text-primary dark:text-white">
-        Approved users
-      </h1>
-      <div className="pt-2 px-4 grid grid-cols-12 gap-3 md:gap-6 2xl:gap-4.5">
-        <Filter officeData={officeData} collegeData={collegeData} />
-        <UserContainer
-          columns={columns}
-          rows={userData}
-        />
-      </div>
+    <div className="bg-white sm:px-6 px-2 dark:bg-black dark:border-black">
+      <Breadcrumb
+        title="Approved Requests"
+        mainRoute="User"
+        subRoute="Approved"
+      />
 
+      <div className="pt-2 px-4 grid grid-cols-12 gap-3 md:gap-6 2xl:gap-4.5">
+        <UserContainer columns={columns} rows={userData} />
+      </div>
     </div>
   );
 };
