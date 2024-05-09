7ddcc5a590716171ea1ef7a1b57df613b593b571
Eyader Tsehayu <eyaderts@gmail.com>
2024-05-09 12:12:39 +0300

    modify  step acadimic staff ui

--- components/Admin/ManageAcademicStaffOffices.jsx
+++ components/Admin/ManageAcademicStaffOffices.jsx
@@ -4,17 +4,17 @@ import AdminContainer from "@/components/Admin/AdminContainer";
 import React, { useState, useEffect, useRef } from "react";
 
 import RegisterOffice from "@/components/Modals/RegisterOffice";
-import useSWR from 'swr';
+import useSWR from "swr";
 import { usePathname } from "next/navigation";
 import Modal from "@mui/material/Modal";
 import { toast } from "react-toastify";
-import SwapVertIcon from '@mui/icons-material/SwapVert';
-import KeyboardDoubleArrowUpIcon from '@mui/icons-material/KeyboardDoubleArrowUp';
-import KeyboardDoubleArrowDownIcon from '@mui/icons-material/KeyboardDoubleArrowDown';
-import ArrowCircleDownIcon from '@mui/icons-material/ArrowCircleDown';
-import AddIcon from '@mui/icons-material/Add';
+import SwapVertIcon from "@mui/icons-material/SwapVert";
+import KeyboardDoubleArrowUpIcon from "@mui/icons-material/KeyboardDoubleArrowUp";
+import KeyboardDoubleArrowDownIcon from "@mui/icons-material/KeyboardDoubleArrowDown";
+import ArrowCircleDownIcon from "@mui/icons-material/ArrowCircleDown";
+import AddIcon from "@mui/icons-material/Add";
 
-import HorizontalRuleIcon from '@mui/icons-material/HorizontalRule';
+import HorizontalRuleIcon from "@mui/icons-material/HorizontalRule";
 // import { MaterialCommunityIcons } from '@expo/vector-icons';
 
 // const PersonItem = ({person,index }) => (
@@ -30,8 +30,6 @@ import HorizontalRuleIcon from '@mui/icons-material/HorizontalRule';
 //   </>
 // );
 
-
-
 const ManageAcademicStaffOffices = () => {
   const [keyValuePairs, setKeyValuePairs] = useState({});
   const [selectedRows, setSelectedRows] = useState([]);
@@ -43,65 +41,59 @@ const ManageAcademicStaffOffices = () => {
       handleClose();
     }
   };
-  
+
   const [steps, setSteps] = useState({});
- 
+
   // const [steps, setSteps] = useState({});
   const [selectedKey, setSelectedKey] = useState(null); // State for selected key
   //  setSteps({"yes":["College Dean"]})
   const [stepData, setStepData] = useState([]);
-  const [stepError, setStepError] = useState(null)
- 
-
-  
+  const [stepError, setStepError] = useState(null);
 
   useEffect(() => {
-    
-  
     const fetchData = async () => {
       try {
         const stepType = "ACADEMIC"; // Define your stepType here
         const url = "/api/steps"; // Define the URL
-  
+
         // Construct URL with query parameter
         const fullUrl = `${url}?stepType=${stepType}`;
-  
+
         // Make the GET request using fetch
         const response = await fetch(fullUrl);
-  
+
         // Check if response is successful
         if (!response.ok) {
           throw new Error("Network response was not ok");
         }
-  
+
         // Extract JSON data from response
         const data = await response.json();
-  
+
         // Update data format if needed
         const updatedData = data.map((user) => ({
           ...user,
           id: user._id,
         }));
-  
+
         // Set data into state
         setStepData(data);
-  
+
         console.log("setDraggedData", data);
       } catch (error) {
         // Handle errors
         setStepError(error);
       }
     };
-  
+
     // Call fetchData when component mounts
     fetchData();
-  
+
     // No cleanup or dependency array needed as we only want to fetch data once
   }, []);
-  
-  if (stepData) {
 
-    console.log("stepData ", stepData[0])
+  if (stepData) {
+    console.log("stepData ", stepData[0]);
   }
 
   // Render loading state
@@ -111,14 +103,12 @@ const ManageAcademicStaffOffices = () => {
 
   const list = [];
   console.log("stepData[0].name", stepData[0]?.name);
-  
+
   const data = stepData[0];
 
   for (let index = 0; index < stepData.length; index++) {
-
-    Object.keys(data).forEach(key => {
-    
-      if (key === 'name') {
+    Object.keys(data).forEach((key) => {
+      if (key === "name") {
         list.push(data[key]);
       }
     });
@@ -126,23 +116,23 @@ const ManageAcademicStaffOffices = () => {
   const values = stepData[0]?.nextSteps;
 
   const modifySteps = async (key, value) => {
-    setSteps(prevSteps => ({
+    setSteps((prevSteps) => ({
       ...prevSteps,
       newProperty: ["New Value"],
       // You can add more properties here if needed
     }));
-   
+
     try {
       const response = await fetch("/api/steps", {
         method: "PATCH",
         body: JSON.stringify({
           key,
           value,
-          stepType: "STAFF"
+          stepType: "STAFF",
         }),
       });
       if (response.ok) {
-        toast.success("Steps updated successfully!")
+        toast.success("Steps updated successfully!");
         console.log("Steps updated successfully!");
         console.log("keyValuePairs", keyValuePairs);
         // Optionally, you can redirect or show a success message here
@@ -154,8 +144,7 @@ const ManageAcademicStaffOffices = () => {
       console.error("Error creating steps:", error);
       // Handle any unexpected errors
     }
-
-  }
+  };
 
   // const keyValuePairs = {};
   stepData.forEach((data, index) => {
@@ -163,18 +152,16 @@ const ManageAcademicStaffOffices = () => {
   });
 
   const addItem = (key, value) => {
-    keyValuePairs[key].push(value)
-    // console.log("ttttttttttttttttttttt","yyy",value);
+    keyValuePairs[key].push(value);
+
     if (key !== value && key !== "Select a Step") {
-      setKeyValuePairs(prevKeyValuePairs => {
+      setKeyValuePairs((prevKeyValuePairs) => {
         const updatedPairs = { ...prevKeyValuePairs };
         updatedPairs[key] = [...(updatedPairs[key] || []), value];
         return updatedPairs;
       });
     }
-    // console.log("ttttttttttttttttttttt","yyy",keyValuePairs);
   };
-
 
   const removeItem = (key, value) => {
     keyValuePairs[key].pop(value);
@@ -183,11 +170,6 @@ const ManageAcademicStaffOffices = () => {
       [key]: prevSteps[key]?.filter((item) => item !== value),
     }));
   };
-
-
-  
-
-
 
   // Iterate over the key-value pairs of the object
   Object.entries(keyValuePairs).forEach(([key, value]) => {
@@ -200,8 +182,8 @@ const ManageAcademicStaffOffices = () => {
   const Approved = {
     key: "APPROVED",
     name: "APPROVED",
-    nextSteps: []
-  }
+    nextSteps: [],
+  };
   return (
     <div
       className={`rounded-lg border border-stroke bg-white px-5 pt-7.5 pb-5 shadow-default dark:border-strokedark dark:bg-boxdark sm:px-7.5  
@@ -209,9 +191,7 @@ const ManageAcademicStaffOffices = () => {
     >
       <div className="flex-grow"></div>
       <div className="flex w-full justify-between items-center mb-4">
-        <div className="flex w-1/3 ">
-
-        </div>
+        <div className="flex w-1/3 "></div>
 
         <div className="flex gap-4 flex-inline  items-center rounded-md  p-1.5 ">
           <button
@@ -224,75 +204,86 @@ const ManageAcademicStaffOffices = () => {
       </div>
       <div className="flex flex-wrap items-start justify-between gap-3 sm:flex-nowrap">
         <div className="flex w-full flex-wrap gap-3 sm:gap-5">
-
-
           <main className="flex min-h-screen flex-col items-center space-y-4">
-
-
-            <select class="ml-1 mr-15  bg-primary text-white  inline-flex items-center gap-2.5 rounded-md dark:bg-boxdark px-5.5 py-3 font-medium  dark:border-strokedark dark:text-white" value={selectedKey} onChange={(e) => setSelectedKey(e.target.value)}>
-
-              <option value={null}>Select a Step</option>
+            <select
+              class="ml-1 mr-15 font-medium text-black dark:text-white text-md font-satoshi   inline-flex items-center gap-2.5 bg-gray border-stroke rounded-md dark:bg-boxdark-2 px-5.5 py-3 "
+              value={selectedKey}
+              onChange={(e) => setSelectedKey(e.target.value)}
+            >
+              <option value={null}>
+                Select an office for adjusting its step
+              </option>
               {stepData.map((data, key) => (
                 <option key={key} value={data.name}>
                   {data.name}
-
                 </option>
               ))}
             </select>
             <div className="flex flex-col gap-15 md:flex-row">
-
               {/* && !stepData[selectedKey]?.includes(value.name )  */}
 
               {/* List of available values to add (Right Box - Minus) */}
               <div className="mt-5">
                 <ul className="pl-5 py-3  ">
-                  {Object.entries(keyValuePairs).map(([key, value]) => (
-                    (key !== selectedKey && !keyValuePairs[selectedKey]?.includes(key)) && (
-                      <div key={key}>
-
-                        <li className="relative w-1/1.5 flex space-x-3 border rounded p-2 bg-gray-100 mb-5 mr-3">
-                          <div className="justify-center">{key}</div>
-                          <button className="flex flex-1 justify-end" onClick={() => addItem(selectedKey, key)}>
+                  {Object.entries(keyValuePairs).map(
+                    ([key, value]) =>
+                      key !== selectedKey &&
+                      !keyValuePairs[selectedKey]?.includes(key) && (
+                        <div key={key}>
+                          <li className="relative w-1/1.5 text-black dark:text-white text-md font-satoshi font-medium  bg-gray dark:bg-boxdark-2 flex space-x-3 border-none rounded p-2  mb-5 mr-3 ">
+                            <div className="justify-center">{key}</div>
+                            <button
+                              className="flex flex-1 justify-end"
+                              onClick={() => addItem(selectedKey, key)}
+                            >
+                              {/* <ArrowCircleRightIcon /> */}
+                              <AddIcon />
+                            </button>
+                          </li>
+                        </div>
+                      )
+                  )}
+                  {selectedKey?.toUpperCase() === "HR" &&
+                    !keyValuePairs[selectedKey]?.includes(Approved.name) && (
+                      <div key={Approved.name}>
+                        <li className="relative w-1/1.5 text-black dark:text-white text-md font-satoshi font-medium  bg-gray dark:bg-boxdark-2 flex space-x-3 border-none rounded p-2  mb-5 mr-3 ">
+                          <div className="justify-center">{Approved.name}</div>
+                          <button
+                            className="flex flex-1 justify-end"
+                            onClick={() => addItem(selectedKey, Approved.name)}
+                          >
                             {/* <ArrowCircleRightIcon /> */}
                             <AddIcon />
                           </button>
                         </li>
-
-
-
                       </div>
-                    )
-                  ))}
-                  {selectedKey?.toUpperCase() === "HR" && !keyValuePairs[selectedKey]?.includes(Approved.name) && (
-                    <div key={Approved.name}>
-
-                      <li className="relative w-1/1.5 flex space-x-3 border rounded p-2 bg-gray-100 mb-5 mr-3">
-                        <div className="justify-center">{Approved.name}</div>
-                        <button className="flex flex-1 justify-end" onClick={() => addItem(selectedKey, Approved.name)}>
-                          {/* <ArrowCircleRightIcon /> */}
-                          <AddIcon />
-                        </button>
-                      </li>
-
-
-
-                    </div>
-                  )}
+                    )}
                 </ul>
               </div>
               {/* rounded-md border border-stroke bg-white shadow-default dark:border-black dark:bg-black */}
               {/* remover */}
-              <div className="mt-5 " >
+              {selectedKey && selectedKey !== "Select a Step" && (
+                <p className="text-base font-medium text-center dark:text-stroke text-black bg-gray-2 border-none dark:bg-boxdark-2 leading-10 font-satoshi  text-wrap   line-clamp-3 w-1/4 flex  justify-center  border rounded-lg p-10 bg-gray-100 mb-5 mt-5 ml-10">
+                  Upon clicking the 'Add' button on the left side, the newly
+                  added item becomes the subsequent step following the current
+                  status {selectedKey}.
+                </p>
+              )}
+              <div className="mt-5 ">
                 <ul className="pl-5 py-3">
                   {keyValuePairs[selectedKey]?.map((value) => (
-                    <li className="relative w-1/1.5 flex space-x-3 border rounded p-2 bg-gray-100 mb-5 mr-3" key={value}>
-
-                      <button className="flex flex-1 justify-start" onClick={() => removeItem(selectedKey, value)}>
+                    <li
+                      className="relative w-1/1.5 text-black dark:text-white text-md font-satoshi font-medium  bg-gray dark:bg-boxdark-2 flex space-x-3 border-none rounded p-2 bg-gray-100 mb-5 mr-3"
+                      key={value}
+                    >
+                      <button
+                        className="flex flex-1 justify-start"
+                        onClick={() => removeItem(selectedKey, value)}
+                      >
                         {/* <ArrowCircleLeftIcon /> */}
                         <HorizontalRuleIcon />
                       </button>
                       <div className="justify-center">{value}</div>
-
                     </li>
                   ))}
                 </ul>
@@ -301,19 +292,16 @@ const ManageAcademicStaffOffices = () => {
 
             <div className=" w-full px-1 ">
               <button
-
-                onClick={() => modifySteps(selectedKey, keyValuePairs[selectedKey])}
+                onClick={() =>
+                  modifySteps(selectedKey, keyValuePairs[selectedKey])
+                }
                 className="ml-5 block w-60 rounded border border-primary bg-primary p-3 text-center font-medium text-white transition hover:bg-opacity-90"
               >
                 Save
               </button>
             </div>
           </main>
-
-
-
         </div>
-
       </div>
 
       <Modal
@@ -329,12 +317,8 @@ const ManageAcademicStaffOffices = () => {
           <RegisterOffice />
         </div>
       </Modal>
-
     </div>
   );
 };
 
 export default ManageAcademicStaffOffices;
-
-
-  
